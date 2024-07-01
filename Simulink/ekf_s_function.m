b66225917c23bb308e0f43563eaac464746dccc5
soup1997 <soup1997@naver.com>
2024-07-01 17:12:07 +0900

    0701

--- Simulink/ekf_s_function.m
+++ Simulink/ekf_s_function.m
@@ -30,14 +30,14 @@ function [Fy_flb, Fy_frb, Fy_rlb, Fy_rrb] = dugoff(Vx, Vy, delta1, delta2, m, ga
         mu = 0.9; % road firction coefficient
         Cy = 3600;
         
-        a_fl = -delta1 + atan((Vy + lf*gamma) / Vx);
-        a_fr = -delta2 + atan((Vy + lf*gamma) / Vx);
-        a_rl = atan((Vy - lr*gamma) / Vx);
-        a_rr = atan((Vy - lr*gamma) / Vx);
+        a_fl = -delta1 + atan((Vy + lf*gamma) / Vx + eps);
+        a_fr = -delta2 + atan((Vy + lf*gamma) / Vx + eps);
+        a_rl = atan((Vy - lr*gamma) / Vx + eps);
+        a_rr = atan((Vy - lr*gamma) / Vx + eps);
             
         a = [a_fl, a_fr, a_rl, a_rr];
 
-        lambda = (mu * Fz) ./ (2 * Cy .* abs(tan(a)));
+        lambda = (mu * Fz) ./ (2*Cy.*abs(tan(a)) + eps);
         
         flambda = [0, 0, 0, 0];
 
@@ -90,14 +90,20 @@ function [x_pred, Fy_b] = stateTransitionFunction(x_est, u_c, params, dt)
 
     x1_dot = 1/m * ((Fx_fl*cos(delta1)) + (Fx_fr*cos(delta2)) - (Fy_fl*sin(delta1)) - (Fy_fr*sin(delta2)) + Fx_rl + Fx_rr - (Cav*Vx^2)) + ...
         (Vy*gamma);
+    
     x2_dot = 1/m * ((Fx_fl*sin(delta1)) + (Fx_fr*sin(delta2)) + (Fy_fl*cos(delta1)) + (Fy_fr*cos(delta2)) + Fy_rl + Fy_rr) - ...
         (Vx*gamma);
-    x3_dot = lf/Iz * ((Fx_fl*sin(delta1) + Fx_fr*sin(delta2) + Fy_fl*cos(delta1) + Fy_fr*cos(delta2)) + ...
+    
+    x3_dot = 1/Iz * (lf * (Fx_fl*sin(delta1) + Fx_fr*sin(delta2) + Fy_fl*cos(delta1) + Fy_fr*cos(delta2)) + ...
         (tr * (Fx_fl*cos(delta1) - Fx_fr*cos(delta2) - Fy_fl*sin(delta1) + Fy_fr*sin(delta2))) - ...
         (lr * (Fy_rl + Fy_rr)));
+
     x4_dot = (Vx/sigma) * (-Fy_fl + Fy_flb);
+    
     x5_dot = (Vx/sigma) * (-Fy_fr + Fy_frb);  
+    
     x6_dot = (Vx/sigma) * (-Fy_rl + Fy_rlb);  
+    
     x7_dot = (Vx/sigma) * (-Fy_rr + Fy_rrb); %  have to check what kind of values to insert  
 
     x_pred = x_est + [x1_dot; x2_dot; x3_dot; x4_dot; x5_dot; x6_dot; x7_dot] * dt;  
@@ -116,7 +122,7 @@ function [sys, x0, str, ts] = mdlInitializeSizes
     sys = simsizes(sizes);
 
     P0 = diag([1, 1, 0.0001, 1000, 1000, 1000, 1000]);
-    x0  = [0; 0; 0; 0.01; 0.01; 0.01; 0.01; reshape(P0, [], 1)];  % Initial state and covariance matrix
+    x0  = [0; 0; 0; 0.0; 0.0; 0.0; 0.0; reshape(P0, [], 1)];  % Initial state and covariance matrix
     str = [];
     ts  = [-1 0];  % Sample time
 end
@@ -132,7 +138,7 @@ function sys = mdlUpdate(t, x, u)
     m = (226.26 + 70);  % Mass of the vehicle + 70 kg Load
     Iz = 146.827; % moment of inertia about yaw axis
     radius = 0.262;  % Wheel radius
-    Cav = (0.5 * 1.293 * 3.237 * 1.1);  % Aerodynamic coefficient (0.5 * air_density(=1.293 kg m−3) * air_drag * cross_sectional_area)
+    Cav = (0.5 * 1.205 * 3.237 * 1.1);  % Aerodynamic coefficient (0.5 * air_density(=1.205 kg m−3) * air_drag * cross_sectional_area)
     lf = 0.813;  % Distance from CG to front axle
     lr = 0.787;  % Distance from CG to rear axle
     tr = 1.242;  % Track width
@@ -148,7 +154,7 @@ function sys = mdlUpdate(t, x, u)
     u_c = u(1:6); % control input
     z = u(7:end); % measurement
 
-    Q = diag([1, 1, 0.0001, 1000, 1000, 1000, 1000]); % Process Noise Covariance Matrix
+    Q = diag([1, 1, 0.0001, 100, 100, 100, 100]); % Process Noise Covariance Matrix
     R = diag([0.0001, 0.001, 0.000001, 0.0001, 0.0001]); % Measurement Noise Covariance Matrix 
     
     [x_pred, Fy_b] = stateTransitionFunction(x_est, u_c, params, dt);
@@ -179,17 +185,17 @@ function sys = mdlUpdate(t, x, u)
     h = [x_pred(1);
          x_pred(2);
          x_pred(3);
-         (1/m) * ( (u(2) + u(3))/radius * cos(delta) - (x_pred(4) + x_pred(5)) * sin(delta) + (u(4) + u(5))/radius - Cav * x_pred(1)^2 );
-         (1/m) * ( (u(2) + u(3))/radius * sin(delta) + (x_pred(4) + x_pred(5)) * cos(delta) + x_pred(6) + x_pred(7))];
+         (1/m) * (((u(3) + u(4))/radius*cos(delta)) - ((x_pred(4) + x_pred(5))*sin(delta)) + (u(5) + u(6))/radius - (Cav*x_pred(1)^2));
+         (1/m) * (((u(3) + u(4))/radius*sin(delta)) + ((x_pred(4) + x_pred(5))*cos(delta)) + x_pred(6) + x_pred(7))];
 
     % Update step
-    x_est = x_pred + (K * (z - h));
-    P_est = (eye(7) - K * H) * P_pred;
-
+    %x_est = x_pred + (K * (z - h));
+    %P_est = (eye(7) - K * H) * P_pred;
+    
     % 
     % ack state vector
-    %x_est = x_pred;
-    %P_est = P_pred;
+    x_est = x_pred;
+    P_est = P_pred;
     sys = [x_est; P_est(:)];
 end
 
