0dbb78c69c2b328b3c4b1ca5b7f9da1d1ff5a9b7
JM\jm538 <optimus5383@gmail.com>
2024-09-10 23:11:15 +0900

    conclusion, picture update

--- Simulink/results/TV/out_put_plot.m
+++ Simulink/results/TV/out_put_plot.m
@@ -4,10 +4,7 @@ Md = load("Md.mat");
 time = Md.data.Time;
 values = Md.data.Data;
 
-time = time(50003:150000);
-values = values(50003:150000);
 
-time = time - time(1);
 % Plotting the data
 
 plot(time, values);
@@ -19,13 +16,13 @@ xlabel('t[s]', 'FontSize', 20);
 ylabel('Md[Nm]', 'FontSize', 20);
 
 % range
-xlim([0,100])
+xlim([0,30])
 
 set(gca, 'FontSize', 15); % Set font size for axis tick labels
 
 % Add legend
 legend('Md','FontSize', 15);
-
+saveas(figure(1), 'C:\Users\jm538\Desktop\Code\TV_IITP\Paper\Figures\Md.tiff');
 %% Sinus Test
 % Data import
 sinus_TV = load('C:\Users\jm538\Desktop\Code\TV_IITP\Simulink\results\TV\sinus\Sinus_test_TV.mat');
@@ -38,29 +35,24 @@ r_TV = sinus_TV.data{2}.Values.Data;
 r_No = sinus_No.data.Data;
 
 % plot
-
+figure
 plot(time,r_des);
 
 hold on
 
 plot(time,r_TV)
 
-fontSize = 18;
-legendFontSize = 12;
-xlabel('Time (s)', 'FontSize', fontSize);
-ylabel('yaw rate (rad/s)', 'FontSize', fontSize);
-legend('yaw rate_{desired}', 'yaw rate_{torque vectoring}', 'FontSize', legendFontSize);
-saveas(figure(1), 'C:\Users\jm538\Desktop\Code\TV_IITP\Paper\Figures\yaw_rate_sinus_TV.tiff');
-figure
-plot(time,r_des);
 hold on
+
 plot(time_No,r_No)
+xlim([0,30])
 fontSize = 18;
 legendFontSize = 12;
 xlabel('Time (s)', 'FontSize', fontSize);
 ylabel('yaw rate (rad/s)', 'FontSize', fontSize);
-legend('yaw rate_{desired}','yaw rate_{base}', 'FontSize', legendFontSize);
-saveas(figure(2), 'C:\Users\jm538\Desktop\Code\TV_IITP\Paper\Figures\yaw_rate_sinus_No.tiff');
+legend('yaw rate_{desired}', 'yaw rate_{with TV}','yaw rate_{without TV}', 'FontSize', legendFontSize);
+iptsetpref('ImshowBorder','tight'); 
+saveas(figure(1), 'C:\Users\jm538\Desktop\Code\TV_IITP\Paper\Figures\yaw_rate_sinus.tiff');
 
 
 
@@ -77,29 +69,25 @@ r_TV = steady_TV.data{2}.Values.Data;
 r_No = steady_No.data{1}.Values.Data;
 
 % plot
-
-plot(time,r_des_TV);
+figure
+plot(time_No,r_des_No);
 
 hold on
 plot(time,r_TV)
-
-fontSize = 18;
-legendFontSize = 12;
-xlabel('Time (s)', 'FontSize', fontSize);
-ylabel('yaw rate (rad/s)', 'FontSize', fontSize);
-legend('yaw rate_{desired}', 'yaw rate_{torque vectoring}', 'FontSize', legendFontSize);
-saveas(figure(1), 'C:\Users\jm538\Desktop\Code\TV_IITP\Paper\Figures\yaw_rate_steady_TV.tiff');
-figure
-plot(time_No,r_des_No)
 hold on
 plot(time_No,r_No)
 
 fontSize = 18;
 legendFontSize = 12;
+
+
 xlabel('Time (s)', 'FontSize', fontSize);
 ylabel('yaw rate (rad/s)', 'FontSize', fontSize);
-legend('yaw rate_{desired}', 'yaw rate_{base}', 'FontSize', legendFontSize);
-saveas(figure(1), 'C:\Users\jm538\Desktop\Code\TV_IITP\Paper\Figures\yaw_rate_steady_No.tiff');
+legend('yaw rate_{desired}', 'yaw rate_{with TV}', 'yaw rate_{with TV}', 'FontSize', legendFontSize);
+xlim([0,25])
+
+
+
 
 
 %% race track
