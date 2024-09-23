4be2f90a8b1014dba9378f8c890e54c5adedfc22
soup1997 <soup1997@naver.com>
2024-09-23 19:53:55 +0900

    figure updated

--- Simulink/result_plot.m
+++ Simulink/result_plot.m
@@ -30,7 +30,7 @@ rmse_sinus_rl = sqrt(mean((Fyrl_sinus_est - Fyrl_sinus_true_interp).^2));
 rmse_sinus_rr = sqrt(mean((Fyrr_sinus_est - Fyrr_sinus_true_interp).^2));
 rmse_sinus_my = sqrt(mean((My_sinus_est - My_sinus_true_interp).^2));
 
-fontSize = 18;
+fontSize = 20;
 legendFontSize = 12;
 time_limit = 30;
 sinus_indices = Fy_sinus_time <= time_limit;
@@ -41,55 +41,60 @@ fprintf('Fyfl error: %.2f, Fyfr error: %.2f, Fyrl error: %.2f, Fyrr error: %.2f,
 
 % Plotting with small margins between subplots
 figure(1);
-set(gcf, 'Position', [100, 100, 1000, 800]);
+set(gcf, 'Position', [100, 100, 1000, 1000]);
 tiledlayout(2, 1, 'Padding', 'compact', 'TileSpacing', 'compact');
 
-nexttile;
-plot(Fy_sinus_time(sinus_indices), Fyfl_sinus_true_interp(sinus_indices)); hold on;
-plot(Fy_sinus_time(sinus_indices), Fyfl_sinus_est(sinus_indices)); hold off;
+ax1 = nexttile;
+plot(Fy_sinus_time(sinus_indices), Fyfl_sinus_true_interp(sinus_indices), 'LineWidth', 1); hold on; 
+plot(Fy_sinus_time(sinus_indices), Fyfl_sinus_est(sinus_indices), 'LineWidth', 1); hold off;
 xlabel('Time (s)', 'FontSize', fontSize);
-ylabel('Fy_{fl} (N)', 'FontSize', fontSize);
-legend('Fy_{fl, Ref}', 'Fy_{fl, Est}', 'FontSize', legendFontSize);
+ylabel('F_{y, FL} (N)', 'FontSize', fontSize);
+legend('GroundTruth', 'Estimation', 'FontSize', legendFontSize);
+set(ax1, 'FontSize', 15);  % Set font size for all text in the axes, including x-ticks
 
-nexttile;
-plot(Fy_sinus_time(sinus_indices), Fyfr_sinus_true_interp(sinus_indices)); hold on;
-plot(Fy_sinus_time(sinus_indices), Fyfr_sinus_est(sinus_indices)); hold off;
+ax2 = nexttile;
+plot(Fy_sinus_time(sinus_indices), Fyfr_sinus_true_interp(sinus_indices), 'LineWidth', 1); hold on;
+plot(Fy_sinus_time(sinus_indices), Fyfr_sinus_est(sinus_indices), 'LineWidth', 1); hold off;
 xlabel('Time (s)', 'FontSize', fontSize);
-ylabel('Fy_{fr} (N)', 'FontSize', fontSize);
-legend('Fy_{fr, Ref}', 'Fy_{fr, Est}', 'FontSize', legendFontSize);
+ylabel('F_{y,FR} (N)', 'FontSize', fontSize);
+legend('GroundTruth', 'Estimation', 'FontSize', legendFontSize);
+set(ax2, 'FontSize', 15);  % Set font size for all text in the axes, including x-ticks
 
 saveas(figure(1), 'C:\Users\SMEET_SIMUL\Desktop\Torque-Vectoring\Paper\Figures\frontForce_sinus.png');
 
 figure(2);
-set(gcf, 'Position', [100, 100, 1000, 800]);
+set(gcf, 'Position', [100, 100, 1000, 1000]);
 tiledlayout(2, 1, 'Padding', 'compact', 'TileSpacing', 'compact');
 
-nexttile;
-plot(Fy_sinus_time(sinus_indices), Fyrl_sinus_true_interp(sinus_indices)); hold on;
-plot(Fy_sinus_time(sinus_indices), Fyrl_sinus_est(sinus_indices)); hold off;
+ax3 = nexttile;
+plot(Fy_sinus_time(sinus_indices), Fyrl_sinus_true_interp(sinus_indices), 'LineWidth', 1); hold on;
+plot(Fy_sinus_time(sinus_indices), Fyrl_sinus_est(sinus_indices), 'LineWidth', 1); hold off;
 xlabel('Time (s)', 'FontSize', fontSize);
-ylabel('Fy_{rl} (N)', 'FontSize', fontSize);
-legend('Fy_{rl, Ref}', 'Fy_{rl, Est}', 'FontSize', legendFontSize);
+ylabel('F_{y,RL} (N)', 'FontSize', fontSize);
+legend('GroundTruth', 'Estimation', 'FontSize', legendFontSize);
+set(ax3, 'FontSize', 15);  % Set font size for all text in the axes, including x-ticks
 
-nexttile;
-plot(Fy_sinus_time(sinus_indices), Fyrr_sinus_true_interp(sinus_indices)); hold on;
-plot(Fy_sinus_time(sinus_indices), Fyrr_sinus_est(sinus_indices)); hold off;
+ax4 = nexttile;
+plot(Fy_sinus_time(sinus_indices), Fyrr_sinus_true_interp(sinus_indices), 'LineWidth', 1); hold on;
+plot(Fy_sinus_time(sinus_indices), Fyrr_sinus_est(sinus_indices), 'LineWidth', 1); hold off;
 xlabel('Time (s)', 'FontSize', fontSize);
-ylabel('Fy_{rr} (N)', 'FontSize', fontSize);
-legend( 'Fy_{rr, Ref}', 'Fy_{rr, Est}', 'FontSize', legendFontSize);
+ylabel('F_{y, RR} (N)', 'FontSize', fontSize);
+legend('GroundTruth', 'Estimation', 'FontSize', legendFontSize);
+set(ax4, 'FontSize', 15);  % Set font size for all text in the axes, including x-ticks
 
 saveas(figure(2), 'C:\Users\SMEET_SIMUL\Desktop\Torque-Vectoring\Paper\Figures\rearForce_sinus.png');
 
 figure(3);
-set(gcf, 'Position', [100, 100, 1000, 400]);
+set(gcf, 'Position', [100, 100, 1000, 500]);
 tiledlayout(1, 1, 'Padding', 'compact', 'TileSpacing', 'compact'); 
 
-nexttile;
-plot(My_sinus_est_time(My_sinus_indices), My_sinus_true_interp(My_sinus_indices)); hold on;
-plot(My_sinus_est_time(My_sinus_indices), My_sinus_est(My_sinus_indices)); hold off;
+ax5 = nexttile;
+plot(My_sinus_est_time(My_sinus_indices), My_sinus_true_interp(My_sinus_indices), 'LineWidth', 1); hold on;
+plot(My_sinus_est_time(My_sinus_indices), My_sinus_est(My_sinus_indices), 'LineWidth', 1); hold off;
 xlabel('Time (s)','FontSize', fontSize);
 ylabel('M_{y} (N.m)', 'FontSize', fontSize);
-legend('M_{y, Ref}', 'M_{y, Est}', 'FontSize', legendFontSize);
+legend('GroundTruth', 'Estimation', 'FontSize', legendFontSize);
+set(ax5, 'FontSize', 15);  % Set font size for all text in the axes, including x-ticks
 
 saveas(figure(3), 'C:\Users\SMEET_SIMUL\Desktop\Torque-Vectoring\Paper\Figures\My_sinus.png');
 
@@ -135,55 +140,60 @@ My_steady_indices = My_steady_est_time <= time_limit;
 
 % Plotting with small margins between subplots
 figure(4);
-set(gcf, 'Position', [100, 100, 1000, 800]);
+set(gcf, 'Position', [100, 100, 1000, 1000]);
 tiledlayout(2, 1, 'Padding', 'compact', 'TileSpacing', 'compact');
 
-nexttile;
-plot(Fy_steady_time(steady_indices), Fyfl_steady_true_interp(steady_indices)); hold on;
-plot(Fy_steady_time(steady_indices), Fyfl_steady_est(steady_indices)); hold off;
+ax6 = nexttile;
+plot(Fy_steady_time(steady_indices), Fyfl_steady_true_interp(steady_indices), 'LineWidth', 1); hold on;
+plot(Fy_steady_time(steady_indices), Fyfl_steady_est(steady_indices), 'LineWidth', 1); hold off;
 xlabel('Time (s)', 'FontSize', fontSize);
-ylabel('Fy_{fl} (N)', 'FontSize', fontSize);
-legend('Fy_{fl, Ref}', 'Fy_{fl, Est}', 'FontSize', legendFontSize);
+ylabel('F_{y, FL} (N)', 'FontSize', fontSize);
+legend('GroundTruth', 'Estimation', 'FontSize', legendFontSize);
+set(ax6, 'FontSize', 15);  % Set font size for all text in the axes, including x-ticks
 
-nexttile;
-plot(Fy_steady_time(steady_indices), Fyfr_steady_true_interp(steady_indices)); hold on;
-plot(Fy_steady_time(steady_indices), Fyfr_steady_est(steady_indices)); hold off;
+ax7 = nexttile;
+plot(Fy_steady_time(steady_indices), Fyfr_steady_true_interp(steady_indices), 'LineWidth', 1); hold on;
+plot(Fy_steady_time(steady_indices), Fyfr_steady_est(steady_indices), 'LineWidth', 1); hold off;
 xlabel('Time (s)', 'FontSize', fontSize);
-ylabel('Fy_{fr} (N)', 'FontSize', fontSize);
-legend('Fy_{fr, Ref}', 'Fy_{fr, Est}', 'FontSize', legendFontSize);
+ylabel('F_{y, FR} (N)', 'FontSize', fontSize);
+legend('GroundTruth', 'Estimation', 'FontSize', legendFontSize);
+set(ax7, 'FontSize', 15);  % Set font size for all text in the axes, including x-ticks
 
 saveas(figure(4), 'C:\Users\SMEET_SIMUL\Desktop\Torque-Vectoring\Paper\Figures\frontForce_steady.png');
 
 figure(5);
-set(gcf, 'Position', [100, 100, 1000, 800]);
+set(gcf, 'Position', [100, 100, 1000, 1000]);
 tiledlayout(2, 1, 'Padding', 'compact', 'TileSpacing', 'compact');
 
-nexttile;
-plot(Fy_steady_time(steady_indices), Fyrl_steady_true_interp(steady_indices)); hold on;
-plot(Fy_steady_time(steady_indices), Fyrl_steady_est(steady_indices)); hold off;
+ax8 = nexttile;
+plot(Fy_steady_time(steady_indices), Fyrl_steady_true_interp(steady_indices), 'LineWidth', 1); hold on;
+plot(Fy_steady_time(steady_indices), Fyrl_steady_est(steady_indices), 'LineWidth', 1); hold off;
 xlabel('Time (s)', 'FontSize', fontSize);
-ylabel('Fy_{rl} (N)', 'FontSize', fontSize);
-legend('Fy_{rl, Ref}', 'Fy_{rl, Est}', 'FontSize', legendFontSize);
+ylabel('F_{y, RL} (N)', 'FontSize', fontSize);
+legend('GroundTruth', 'Estimation', 'FontSize', legendFontSize);
+set(ax8, 'FontSize', 15);  % Set font size for all text in the axes, including x-ticks
 
-nexttile;
-plot(Fy_steady_time(steady_indices), Fyrr_steady_true_interp(steady_indices)); hold on;
-plot(Fy_steady_time(steady_indices), Fyrr_steady_est(steady_indices)); hold off;
+ax9 = nexttile;
+plot(Fy_steady_time(steady_indices), Fyrr_steady_true_interp(steady_indices), 'LineWidth', 1); hold on;
+plot(Fy_steady_time(steady_indices), Fyrr_steady_est(steady_indices), 'LineWidth', 1); hold off;
 xlabel('Time (s)', 'FontSize', fontSize);
-ylabel('Fy_{rr} (N)', 'FontSize', fontSize);
-legend('Fy_{rr, Ref}', 'Fy_{rr, Est}', 'FontSize', legendFontSize);
+ylabel('F_{y, RR} (N)', 'FontSize', fontSize);
+legend('GroundTruth', 'Estimation', 'FontSize', legendFontSize);
+set(ax9, 'FontSize', 15);  % Set font size for all text in the axes, including x-ticks
 
 saveas(figure(5), 'C:\Users\SMEET_SIMUL\Desktop\Torque-Vectoring\Paper\Figures\rearForce_steady.png');
 
 figure(6);
-set(gcf, 'Position', [100, 100, 1000, 400]);
+set(gcf, 'Position', [100, 100, 1000, 500]);
 tiledlayout(1, 1, 'Padding', 'compact', 'TileSpacing', 'compact'); 
 
-nexttile;
-plot(My_steady_est_time(My_steady_indices), My_steady_true_interp(My_steady_indices)); hold on;
-plot(My_steady_est_time(My_steady_indices), My_steady_est(My_steady_indices)); hold off;
+ax10 = nexttile;
+plot(My_steady_est_time(My_steady_indices), My_steady_true_interp(My_steady_indices), 'LineWidth', 1); hold on;
+plot(My_steady_est_time(My_steady_indices), My_steady_est(My_steady_indices), 'LineWidth', 1); hold off;
 xlabel('Time (s)', 'FontSize', fontSize);
 ylabel('M_{y} (N.m)', 'FontSize', fontSize);
-legend('M_{y, Ref}', 'M_{y, Est}', 'FontSize', legendFontSize);
+legend('GroundTruth', 'Estimation', 'FontSize', legendFontSize);
+set(ax10, 'FontSize', 15);  % Set font size for all text in the axes, including x-ticks
 ylim([-400, 400]);
 saveas(figure(6), 'C:\Users\SMEET_SIMUL\Desktop\Torque-Vectoring\Paper\Figures\My_steady.png');
 
@@ -229,54 +239,59 @@ fprintf('Fyfl error: %.2f, Fyfr error: %.2f, Fyrl error: %.2f, Fyrr error: %.2f,
 
 % Plotting with small margins between subplots
 figure(7);
-set(gcf, 'Position', [100, 100, 1000, 800]);
+set(gcf, 'Position', [100, 100, 1000, 1000]);
 tiledlayout(2, 1, 'Padding', 'compact', 'TileSpacing', 'compact');
 
-nexttile;
-plot(Fy_race_time(race_indices), Fyfl_race_true_interp(race_indices)); hold on;
-plot(Fy_race_time(race_indices), Fyfl_race_est(race_indices)); hold off;
+ax11 = nexttile;
+plot(Fy_race_time(race_indices), Fyfl_race_true_interp(race_indices), 'LineWidth', 1); hold on;
+plot(Fy_race_time(race_indices), Fyfl_race_est(race_indices), 'LineWidth', 1); hold off;
 xlabel('Time (s)', 'FontSize', fontSize);
-ylabel('Fy_{fl} (N)', 'FontSize', fontSize);
-legend('Fy_{fl, Ref}', 'Fy_{fl, Est}', 'FontSize', legendFontSize);
+ylabel('F_{y, FL} (N)', 'FontSize', fontSize);
+legend('GroundTruth', 'Estimation', 'FontSize', legendFontSize);
+set(ax11, 'FontSize', 15);  % Set font size for all text in the axes, including x-ticks
 
-nexttile;
-plot(Fy_race_time(race_indices), Fyfr_race_true_interp(race_indices)); hold on;
-plot(Fy_race_time(race_indices), Fyfr_race_est(race_indices)); hold off;
+ax12 = nexttile;
+plot(Fy_race_time(race_indices), Fyfr_race_true_interp(race_indices), 'LineWidth', 1); hold on;
+plot(Fy_race_time(race_indices), Fyfr_race_est(race_indices), 'LineWidth', 1); hold off;
 xlabel('Time (s)', 'FontSize', fontSize);
-ylabel('Fy_{fr} (N)', 'FontSize', fontSize);
-legend('Fy_{fr, Ref}', 'Fy_{fr, Est}', 'FontSize', legendFontSize);
+ylabel('F_{y, FR} (N)', 'FontSize', fontSize);
+legend('GroundTruth', 'Estimation', 'FontSize', legendFontSize);
+set(ax12, 'FontSize', 15);  % Set font size for all text in the axes, including x-ticks
 
 saveas(figure(7), 'C:\Users\SMEET_SIMUL\Desktop\Torque-Vectoring\Paper\Figures\frontForce_race.png');
 
 figure(8);
-set(gcf, 'Position', [100, 100, 1000, 800]);
+set(gcf, 'Position', [100, 100, 1000, 1000]);
 tiledlayout(2, 1, 'Padding', 'compact', 'TileSpacing', 'compact');
 
-nexttile;
-plot(Fy_race_time(race_indices), Fyrl_race_true_interp(race_indices)); hold on;
-plot(Fy_race_time(race_indices), Fyrl_race_est(race_indices)); hold off;
+ax13 = nexttile;
+plot(Fy_race_time(race_indices), Fyrl_race_true_interp(race_indices),'LineWidth', 1); hold on;
+plot(Fy_race_time(race_indices), Fyrl_race_est(race_indices), 'LineWidth', 1); hold off;
 xlabel('Time (s)', 'FontSize', fontSize);
-ylabel('Fy_{rl} (N)', 'FontSize', fontSize);
-legend('Fy_{rl, Ref}', 'Fy_{rl, Est}', 'FontSize', legendFontSize);
+ylabel('F_{y, RL} (N)', 'FontSize', fontSize);
+legend('GroundTruth', 'Estimation', 'FontSize', legendFontSize);
+set(ax13, 'FontSize', 15);  % Set font size for all text in the axes, including x-ticks
 
-nexttile;
-plot(Fy_race_time(race_indices), Fyrr_race_true_interp(race_indices)); hold on;
-plot(Fy_race_time(race_indices), Fyrr_race_est(race_indices)); hold off;
+ax14 = nexttile;
+plot(Fy_race_time(race_indices), Fyrr_race_true_interp(race_indices), 'LineWidth', 1); hold on;
+plot(Fy_race_time(race_indices), Fyrr_race_est(race_indices), 'LineWidth', 1); hold off;
 xlabel('Time (s)', 'FontSize', fontSize);
-ylabel('Fy_{rr} (N)', 'FontSize', fontSize);
-legend('Fy_{rr, Ref}', 'Fy_{rr, Est}', 'FontSize', legendFontSize);
+ylabel('F_{y, RR} (N)', 'FontSize', fontSize);
+legend('GroundTruth', 'Estimation', 'FontSize', legendFontSize);
+set(ax14, 'FontSize', 15);  % Set font size for all text in the axes, including x-ticks
 
 saveas(figure(8), 'C:\Users\SMEET_SIMUL\Desktop\Torque-Vectoring\Paper\Figures\rearForce_race.png');
 
 figure(9);
-set(gcf, 'Position', [100, 100, 1000, 400]);
+set(gcf, 'Position', [100, 100, 1000, 500]);
 tiledlayout(1, 1, 'Padding', 'compact', 'TileSpacing', 'compact'); 
 
-nexttile;
-plot(My_race_est_time(My_race_indices), My_race_true_interp(My_race_indices)); hold on;
-plot(My_race_est_time(My_race_indices), My_race_est(My_race_indices)); hold off;
+ax15 = nexttile;
+plot(My_race_est_time(My_race_indices), My_race_true_interp(My_race_indices), 'LineWidth', 1); hold on;
+plot(My_race_est_time(My_race_indices), My_race_est(My_race_indices), 'LineWidth', 1); hold off;
 xlabel('Time (s)', 'FontSize', fontSize);
 ylabel('M_{y} (N.m)', 'FontSize', fontSize);
-legend('M_{y, Ref}', 'M_{y, Est}', 'FontSize', legendFontSize);
+legend('GroundTruth', 'Estimation', 'FontSize', legendFontSize);
+set(ax15, 'FontSize', 15);  % Set font size for all text in the axes, including x-ticks
 
 saveas(figure(9), 'C:\Users\SMEET_SIMUL\Desktop\Torque-Vectoring\Paper\Figures\My_race.png');
